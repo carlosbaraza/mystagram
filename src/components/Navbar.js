edcad9cec716c174df0146f62a2defb1034b7a2c
Carlos Baraza Haro <carlos.baraza@ieee.org>
2017-04-04 00:10:58 +0100

    Fix top bar and add body paddings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,20 @@ import './Navbar.css';
 export class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-default">
+      <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button">
-              <i className="fa fa-camera"></i>
-            </button>
-            <a className="navbar-brand" href="#">Mystagram</a>
-            <button type="button">
-              <i className="fa fa-paper-plane"></i>
-            </button>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="navbar-header">
+                <button type="button">
+                  <i className="fa fa-camera"></i>
+                </button>
+                <a className="navbar-brand" href="#">Mystagram</a>
+                <button type="button">
+                  <i className="fa fa-paper-plane"></i>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
